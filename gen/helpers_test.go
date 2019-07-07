367cc253b7b903e8a7722bf4929a2e5d73462213
Bong Nguyen <van.bong@grabtaxi.com>
2019-07-07 20:53:04 +0800

    refactor: move template execution to a helper func

--- gen/helpers_test.go
+++ gen/helpers_test.go
@@ -1,6 +1,7 @@
 package gen
 
 import (
+	"bytes"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -9,4 +10,20 @@ import (
 func Test_isPointer(t *testing.T) {
 	require.True(t, isPointer("*model.Profile"))
 	require.False(t, isPointer("model.Profile"))
+}
+
+func Test_executeTemplate(t *testing.T) {
+	t.Run("without-func", func(t *testing.T) {
+		buf := &bytes.Buffer{}
+		err := executeTemplate("{{ . }}", buf, "data")
+		require.NoError(t, err)
+		require.Equal(t, "data", buf.String())
+	})
+
+	t.Run("with-func", func(t *testing.T) {
+		buf := &bytes.Buffer{}
+		err := executeTemplate("{{ . | isPointer }}", buf, "*data")
+		require.NoError(t, err)
+		require.Equal(t, "true", buf.String())
+	})
 }

087e60970c4d9a198d3c1603bd8b30506a51fa43
Bong Nguyen <van.bong@grabtaxi.com>
2019-07-21 19:08:32 +0800

    minor update

--- testdata/addsvc/doc.go
+++ testdata/addsvc/doc.go
@@ -2,3 +2,4 @@ package addsvc
 
 //go:generate protoc -I./pb -I$P/googleapis --go_out=plugins=grpc:./pb ./pb/addsvc.proto
 //go:generate protoc -I./pb -I$P/googleapis --grpc-gateway_out=logtostderr=true:./pb ./pb/addsvc.proto
+//go:generate kitgen -d=$TESTDATA/pb -i=AddServer -o=$TESTDATA/server/endpoints.go -t=../../templates/tmpl/endpoints.tmpl
